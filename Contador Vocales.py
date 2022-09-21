abdc73f1dd10056f3bff884efde9e152afeca526
El Yankee <114041407+El-Yankee@users.noreply.github.com>
2022-09-20 23:53:47 -0300

    Add files via upload

---
+++ Contador Vocales.py
@@ -0,0 +1,12 @@
+#Contador de Vocales
+
+s = "aaaaaaaa"
+voc = 0
+
+for i in range (len(s)):
+    if s[i] == "a" or s[i] == "e" or s[i] == "i" or s[i] == "o" or s[i] == "u":
+        voc += 1
+    else:
+        voc += 0
+        
+print ("Number of vowels:", voc)
